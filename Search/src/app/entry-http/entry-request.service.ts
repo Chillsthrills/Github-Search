6a5542d9478afd837959f965ca873e75fe1e63cd
Chillsthrills <pwan.wa100@gmail.com>
2019-12-08 20:46:48 +0300

    profile selector added to app.component.html

--- Search/src/app/entry-http/entry-request.service.ts
+++ Search/src/app/entry-http/entry-request.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers } from '@angular/http'; //Fetch data from github
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntryRequestService {
 
-  constructor() { }
+  private username:string;  //usename property for service
+  // private clientid = '';
+  // private clientsecret = '';
+
+  constructor(private http:Http) {  //inject Http Module
+    console.log("service is ready!");
+    this.username = 'Chillsthrills';
+  }
+  getProfileInfo(){          //Function to get data from github API
+    return this.http.get("https://api.github.com/users/Chillsthrills" + this.username + "?client_id=" +
+    this.clientid + "&client_secret=" + this.clientsecret)
+    .map(res => res.json());
+  }
 }
